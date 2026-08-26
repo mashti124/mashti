b91739b0f7d30f46efd4256fb2cd4f6428024091
mashti124 <mashti124@atomicmail.io>
2026-08-26 23:34:27 +0330

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from colo_map import describe_colo
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 APP_VERSION = "1.5.5"
-PANEL_NAME = "StanNG"
+PANEL_NAME = "Mashti"
 TELEGRAM_CONTACT = "https://t.me/rvivl"
 OTA_REPO = "youdidking/stanngv2"
 OTA_HEADERS = {
